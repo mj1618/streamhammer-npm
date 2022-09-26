d909597de2a2f10ff0ecfbb8dfc522747135a422
Matthew James <matt@Matthews-MacBook-Pro.local>
2022-09-26 20:28:59 +0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,71 @@
-export const myPackage = (taco = ''): string => `${taco} from my package`;
+let userApiKey = '';
+
+export interface IPublishEvent {
+  collectionId: string;
+  streamId: string;
+  data: any;
+}
+
+export interface IStoreEventResponse {
+  eventId: string;
+}
+
+export interface IEvent {
+  account_id: string;
+  event_id: string;
+  collection_id: string;
+  stream_id: string;
+  stream_sequence: number;
+  collection_sequence: number;
+  data: any;
+  created_at: string;
+}
+
+export const streamhammer = () => {
+  return {
+    setApiKey: (apiKey: string): void => {
+      userApiKey = apiKey;
+    },
+    storeEvent: async (event: IPublishEvent) => {
+      const shResponse = await fetch('https://streamhammer.io/api/stream', {
+        method: 'post',
+        body: JSON.stringify(event),
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userApiKey}`,
+        },
+      });
+      const json = await shResponse.json();
+      return json as IStoreEventResponse;
+    },
+    readFromCollection: async (
+      collectionId: string,
+      fromCollectionSequenceNumber = 0
+    ) => {
+      const shResponse = await fetch(
+        `https://streamhammer.io/api/collection/${collectionId}?fromCollectionSequenceNumber=${fromCollectionSequenceNumber}`,
+        {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${userApiKey}`,
+          },
+        }
+      );
+      return (await shResponse.json()) as IEvent[];
+    },
+    readFromStream: async (streamId: string, fromStreamSequenceNumber = 0) => {
+      const shResponse = await fetch(
+        `https://streamhammer.io/api/collection/${streamId}?fromStreamSequenceNumber=${fromStreamSequenceNumber}`,
+        {
+          method: 'get',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${userApiKey}`,
+          },
+        }
+      );
+      return (await shResponse.json()) as IEvent[];
+    },
+  };
+};
