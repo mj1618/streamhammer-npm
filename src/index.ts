997b97f2accbcfe7a4ba385b9acb865084d28465
Matthew James <matthew.stephen.james@gmail.com>
2022-09-26 20:48:04 +0800

    1.3.0

--- src/index.ts
+++ src/index.ts
@@ -15,51 +15,49 @@ export interface IEvent {
   created_at: string;
 }
 
-export const streamhammer = () => {
-  return {
-    setApiKey: (apiKey: string): void => {
-      userApiKey = apiKey;
-    },
-    storeEvent: async (collectionId: string, streamId: string, data: any) => {
-      const shResponse = await fetch('https://streamhammer.io/api/stream', {
-        method: 'post',
-        body: JSON.stringify(event),
+export const streamhammer = {
+  setApiKey: (apiKey: string): void => {
+    userApiKey = apiKey;
+  },
+  storeEvent: async (collectionId: string, streamId: string, data: any) => {
+    const shResponse = await fetch('https://streamhammer.io/api/stream', {
+      method: 'post',
+      body: JSON.stringify(event),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${userApiKey}`,
+      },
+    });
+    const json = await shResponse.json();
+    return json as IStoreEventResponse;
+  },
+  readFromCollection: async (
+    collectionId: string,
+    fromCollectionSequenceNumber = 0
+  ) => {
+    const shResponse = await fetch(
+      `https://streamhammer.io/api/collection/${collectionId}?fromCollectionSequenceNumber=${fromCollectionSequenceNumber}`,
+      {
+        method: 'get',
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${userApiKey}`,
         },
-      });
-      const json = await shResponse.json();
-      return json as IStoreEventResponse;
-    },
-    readFromCollection: async (
-      collectionId: string,
-      fromCollectionSequenceNumber = 0
-    ) => {
-      const shResponse = await fetch(
-        `https://streamhammer.io/api/collection/${collectionId}?fromCollectionSequenceNumber=${fromCollectionSequenceNumber}`,
-        {
-          method: 'get',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${userApiKey}`,
-          },
-        }
-      );
-      return (await shResponse.json()) as IEvent[];
-    },
-    readFromStream: async (streamId: string, fromStreamSequenceNumber = 0) => {
-      const shResponse = await fetch(
-        `https://streamhammer.io/api/collection/${streamId}?fromStreamSequenceNumber=${fromStreamSequenceNumber}`,
-        {
-          method: 'get',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${userApiKey}`,
-          },
-        }
-      );
-      return (await shResponse.json()) as IEvent[];
-    },
-  };
+      }
+    );
+    return (await shResponse.json()) as IEvent[];
+  },
+  readFromStream: async (streamId: string, fromStreamSequenceNumber = 0) => {
+    const shResponse = await fetch(
+      `https://streamhammer.io/api/collection/${streamId}?fromStreamSequenceNumber=${fromStreamSequenceNumber}`,
+      {
+        method: 'get',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${userApiKey}`,
+        },
+      }
+    );
+    return (await shResponse.json()) as IEvent[];
+  },
 };
